c0a8a511c05246ad5e9e641dd2512ec85df0fa23
Senhung Wong <alexwongsenhung@gmail.com>
2018-05-06 00:12:08 -0700

    Project Template

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,14 +2,19 @@
     <div>
         <h2>Projects</h2>
         <div class="center-flex">
-
+            <project></project>
+            <project></project>
         </div>
     </div>
 </template>
 
 <script>
-export default {
+import Project from '../templates/Project';
 
+export default {
+    components: {
+        Project
+    }
 }
 </script>
 
---
+++ src/templates/Project.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <div @mouseover="active = true" @mouseleave="active = false">Hover over me!</div>
+        <div class="animated bounceIn" v-if="active">Show</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            active: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
